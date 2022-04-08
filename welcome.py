6628336b5f570d757078ee3b1598b34c97b6368e
unknown <chavan.ajyesh01@gmail.com>
2022-04-08 18:48:33 +0530

    added find odd no code

--- welcome.py
+++ welcome.py
@@ -4,3 +4,5 @@ print('welcome to python programming language');
 num= int(input(" enter any number to test whether they are even or odd"))
 if (num %2)==0:
 	print( " the number is even");
+else
+	print( " the number is odd");
